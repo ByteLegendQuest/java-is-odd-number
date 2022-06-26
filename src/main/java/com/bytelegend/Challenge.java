a256647c46e931641fc820af5db802e9e0596ab0
karolk75 <karolk410410@gmail.com>
2022-06-26 18:55:13 +0000

    Update at 2022-06-26T18:55:13.842150Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,7 @@
 package com.bytelegend;
 
+import java.lang.Math;
+
 public class Challenge {
     public static void main(String[] args) {
         System.out.println("0 is : " + isOdd(0));
@@ -13,6 +15,6 @@ public class Challenge {
      * otherwise.
      */
     public static String isOdd(int n) {
-        return (n % 2 == 1) ? "odd" : "even";
+        return (Math.abs(n) % 2 == 1) ? "odd" : "even";
     }
 }

46b1bf5289e0a037f8b67b6d101fdc1c649b9f07
Birlinha <omenaribeiro13@gmail.com>
2022-07-15 01:16:51 +0000

    Update at 2022-07-15T01:16:51.426441Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,7 +13,7 @@ public class Challenge {
      * otherwise.
      */
     public static String isOdd(int n) {
-        if (n % 2 == 1) {
+        if (Math.abs(n) % 2 == 1) {
             return "odd";
         } else {
             return "even";

69f29553080417a2de6b19ff3515998de7e68af5
SaraadKun <35584589+SaraadKun@users.noreply.github.com>
2022-07-30 12:40:40 +0000

    Update at 2022-07-30T12:40:40.660089Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,6 +13,6 @@ public class Challenge {
      * otherwise.
      */
     public static String isOdd(int n) {
-        return n & 1 == 1 ? "odd" : "even";
+        return (n & 1 == 1) ? "odd" : "even";
     }
 }
